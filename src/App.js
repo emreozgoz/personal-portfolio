37080845aef67bd2e838972fedea1623c158b7a6
Emre <emreozgoz@gmail.com>
2023-01-25 22:39:03 +0300

    feat: banner created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { NavBar } from './component/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Banner } from './component/banner';
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Banner/>
     </div>
   );
 }
