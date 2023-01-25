5238d3f5391730c3ce90c44d466f53caf9d1a81e
Emre <emreozgoz@gmail.com>
2023-01-25 22:50:40 +0300

    feat: skill component created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { NavBar } from './component/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from './component/banner';
+import { Skills } from './component/skills';
 function App() {
   return (
     <div className="App">
      <NavBar/>
      <Banner/>
+     <Skills/>
     </div>
   );
 }
