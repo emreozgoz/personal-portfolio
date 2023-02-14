eacbfc515b7305e27fd80303aad9ae817569ea26
Emre <emreozgoz@gmail.com>
2023-02-14 23:12:17 +0300

    feat: project component created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { NavBar } from './component/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from './component/banner';
 import { Skills } from './component/skills';
+import { Projects } from './component/projects';
 function App() {
   return (
     <div className="App">
-     <NavBar/>
-     <Banner/>
-     <Skills/>
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
     </div>
   );
 }
