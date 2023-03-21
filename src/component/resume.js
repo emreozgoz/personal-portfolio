52a32ca2107483a26fe88ec55d2b003da6913fd0
Emre <emreozgoz@gmail.com>
2023-03-21 18:44:24 +0300

    fix: resume button

--- src/component/resume.js
+++ src/component/resume.js
@@ -2,24 +2,15 @@ import { Col, Container, Row, Tab, Nav, Button } from "react-bootstrap";
 import Pdf from "../assets/emreozgozcvv.pdf";
 export const Resume = () => {
   return (
-    <section className="project" id="projects">
+    <section className="resume" id="resume">
       <Container>
         <h2>RESUME - CV</h2>
         <p>You can check my CV!</p>
-        <Tab.Container id="projects-tabs" defaultActiveKey="second">
-          <Nav
-            variant="pills"
-            className="mb-5 justify-content-center align-items-center"
-            id="pills-tab"
-          >
-            <Nav.Item>
-              <Nav.Link href={Pdf} eventKey="second">
-                Resume - CV
-              </Nav.Link>
-            </Nav.Item>
-          </Nav>
-          <Tab.Content></Tab.Content>
-        </Tab.Container>
+        <div className="d-grid">
+          <Button  href={Pdf} size="lg">
+            RESUME - CV
+          </Button>
+        </div>
       </Container>
     </section>
   );
